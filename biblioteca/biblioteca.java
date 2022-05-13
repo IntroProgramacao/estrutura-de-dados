bbfe886d1124dd4df4d976a6f5ef40aab84e2bfc
Lucasitron <90011211+Lucasitron@users.noreply.github.com>
2022-05-13 16:17:48 -0300

    Add files via upload

---
+++ biblioteca/biblioteca.java
@@ -0,0 +1,6 @@
+public class biblioteca{
+    //vetor de 100 posições de 
+    Livro[] = new livro[100];
+    
+    public void CadastrarLivro
+}
---
+++ biblioteca/principal.java
@@ -0,0 +1,5 @@
+package biblioteca;
+
+public static void main(String [] args) {
+    
+}
